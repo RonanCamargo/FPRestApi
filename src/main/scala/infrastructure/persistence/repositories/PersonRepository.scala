f1dcf7c330e28f460c2732772232e9bce9a02e70
Ronan Camargo <ronan.cam@gmail.com>
2021-07-12 23:25:58 -0300

    Reformat PersonRepository

--- src/main/scala/infrastructure/persistence/repositories/PersonRepository.scala
+++ src/main/scala/infrastructure/persistence/repositories/PersonRepository.scala
@@ -8,26 +8,20 @@ import slick.jdbc.H2Profile.api._
 import cats.syntax.option._
 import scala.concurrent.ExecutionContext
 
-class PersonRepository(implicit executionContext: ExecutionContext)
+case class PersonRepository(implicit executionContext: ExecutionContext)
     extends FPersonRepository[DBIO, Int, PersonRow] {
 
   private lazy val persons = PersonTable.table
 
-  private def saveAndGetId(person: PersonRow): DBIO[Int] =
-    (persons returning persons.map(_.id)) += person
+  def save(person: PersonRow): DBIO[PersonRow] = saveAndGetId(person).map { id => person.focus(_.id).replace(id.some) }
 
-  def save(
-      person: PersonRow
-  ): DBIO[PersonRow] =
-    saveAndGetId(person)
-      .map { id => person.focus(_.id).replace(id.some) }
+  def find(id: Int): DBIO[Option[PersonRow]] = filterById(id).take(num = 1).result.headOption
 
-  def find(id: Int): DBIO[Option[PersonRow]] =
-    persons.filter(_.id === id).take(num = 1).result.headOption
+  def update(person: PersonRow): DBIO[Int] = persons.update(person)
 
-  def update(person: PersonRow): DBIO[Int] =
-    persons.update(person)
+  def delete(id: Int): DBIO[Int] = filterById(id).delete
 
-  def delete(id: Int): DBIO[Int] = persons.filter(_.id === id).delete
+  private def saveAndGetId(person: PersonRow): DBIO[Int] = (persons returning persons.map(_.id)) += person
 
+  private def filterById(id: Int): Query[PersonTable, PersonRow, Seq] = persons.filter(_.id === id)
 }
