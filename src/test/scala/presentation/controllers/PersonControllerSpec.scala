5dca48cb8a7a6c5c346551bc701136050c6e0edc
Ronan Camargo <ronan.cam@gmail.com>
2021-07-12 22:58:28 -0300

    Update PersonController unit tests

--- src/test/scala/presentation/controllers/PersonControllerSpec.scala
+++ src/test/scala/presentation/controllers/PersonControllerSpec.scala
@@ -18,55 +18,43 @@ import slick.dbio.DBIO
 class PersonControllerSpec extends AnyWordSpec with Matchers with MockFactory {
 
   private val personRepository = stub[PersonRepository]
-  private val dbRunner = mock[DatabaseRunner]
+  private val dbRunner         = stub[DatabaseRunner]
 
   private val subject = PersonController(personRepository)(dbRunner)
 
   "#get" when {
     "asking for an existent person" should {
       "return an Ok response" in {
-        val expectedPerson: Option[PersonRow] =
-          PersonRow(Some(1), "Ronan", 27).some
-        val personId = 1
+        val personId             = 1
+        val request: Request[IO] = Request[IO](method = Method.GET, Uri.unsafeFromString(s"/$personId"))
 
-        val request: Request[IO] =
-          Request[IO](method = Method.GET, Uri.unsafeFromString(s"/$personId"))
+        val expected: PersonRow = PersonRow(personId.some, "Ronan", 27)
 
-        (dbRunner
-          .run(_: DBIO[Option[PersonRow]]))
-          .expects(*)
-          .returning(IO.pure(expectedPerson))
+        (dbRunner.run(_: DBIO[Option[PersonRow]])).when(*).returns(IO.pure(expected.some))
 
-        val result: Response[IO] =
-          subject.routes.orNotFound.run(request).unsafeRunSync
+        val result: Response[IO] = subject.routes.orNotFound.run(request).unsafeRunSync
 
         result.status shouldBe Status.Ok
-        result.as[PersonRow].unsafeRunSync() shouldBe expectedPerson.get
-
+        result.as[PersonRow].unsafeRunSync() shouldBe expected
       }
     }
   }
 
   "#post" when {
     "passing a valid person" should {
-      "" in {
-        val expectedPerson: PersonRow =
-          PersonRow(1.some, "Ronan", 27)
+      "create it successfully" in {
+        val personId                  = 1
+        val expectedPerson: PersonRow = PersonRow(personId.some, "Ronan", 27)
 
         val request: Request[IO] =
           Request[IO](method = Method.POST).withEntity(expectedPerson)
 
-        (dbRunner
-          .run(_: DBIO[PersonRow]))
-          .expects(*)
-          .returning(IO.pure(expectedPerson))
+        (dbRunner.run(_: DBIO[PersonRow])).when(*).returns(IO.pure(expectedPerson))
 
-        val result: Response[IO] =
-          subject.routes.orNotFound.run(request).unsafeRunSync
+        val result: Response[IO] = subject.routes.orNotFound.run(request).unsafeRunSync
 
         result.status shouldBe Status.Created
         result.as[PersonRow].unsafeRunSync() shouldBe expectedPerson
-
       }
     }
   }
