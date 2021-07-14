81c9f19e6f4890079afe165353d9b2f584460507
Ronan Camargo <ronan.cam@gmail.com>
2021-07-13 22:12:10 -0300

    Rename repository

---
+++ src/main/scala/domain/repositories/Repository.scala
@@ -0,0 +1,17 @@
+package domain.repositories
+
+/** Repository for CRUD operations
+  * @tparam F Effect type
+  * @tparam Id Identifier
+  * @tparam E Entity
+  */
+trait Repository[F[_], Id, E] {
+
+  def find(id: Id): F[Option[E]]
+
+  def save(entity: E): F[E]
+
+  def update(entity: E): F[Int]
+
+  def delete(id: Id): F[Int]
+}
--- src/main/scala/infrastructure/persistence/repositories/PersonRepository.scala
+++ src/main/scala/infrastructure/persistence/repositories/PersonRepository.scala
@@ -1,6 +1,6 @@
 package infrastructure.persistence.repositories
 
-import domain.repositories.FPersonRepository
+import domain.repositories.Repository
 import infrastructure.persistence.entities.PersonRow
 import infrastructure.persistence.tables.PersonTable
 import monocle.syntax.all._
@@ -9,19 +9,23 @@ import cats.syntax.option._
 import scala.concurrent.ExecutionContext
 
 case class PersonRepository()(implicit executionContext: ExecutionContext)
-    extends FPersonRepository[DBIO, Int, PersonRow] {
+    extends Repository[DBIO, Int, PersonRow] {
 
   private lazy val persons = PersonTable.table
 
-  def save(person: PersonRow): DBIO[PersonRow] = saveAndGetId(person).map { id => person.focus(_.id).replace(id.some) }
+  def save(person: PersonRow): DBIO[PersonRow] =
+    saveAndGetId(person).map { id => person.focus(_.id).replace(id.some) }
 
-  def find(id: Int): DBIO[Option[PersonRow]] = filterById(id).take(num = 1).result.headOption
+  def find(id: Int): DBIO[Option[PersonRow]] =
+    filterById(id).take(num = 1).result.headOption
 
   def update(person: PersonRow): DBIO[Int] = persons.update(person)
 
   def delete(id: Int): DBIO[Int] = filterById(id).delete
 
-  private def saveAndGetId(person: PersonRow): DBIO[Int] = (persons returning persons.map(_.id)) += person
+  private def saveAndGetId(person: PersonRow): DBIO[Int] =
+    (persons returning persons.map(_.id)) += person
 
-  private def filterById(id: Int): Query[PersonTable, PersonRow, Seq] = persons.filter(_.id === id)
+  private def filterById(id: Int): Query[PersonTable, PersonRow, Seq] =
+    persons.filter(_.id === id)
 }
